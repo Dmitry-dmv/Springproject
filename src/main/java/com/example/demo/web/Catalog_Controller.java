80b0509fd93ae5b7a2eeb57ba6489c99391b6b6e
Dmitrii Chernyh <80121014+Dmitry-dmv@users.noreply.github.com>
2021-11-09 18:48:25 +0300

    Update Catalog_Controller.java
    Correction

--- src/main/java/com/example/demo/web/Catalog_Controller.java
+++ src/main/java/com/example/demo/web/Catalog_Controller.java
@@ -9,35 +9,34 @@ import org.springframework.web.bind.annotation.*;
 @RestController("/catalog")
 public class Catalog_Controller {
        private final ICatalogService catalogService;
-//
-//
-//    @GetMapping(path = "/{id}")
-//    public @ResponseBody
-//    String getById(@PathVariable Integer id) {
-//        return "enter your username"+id;
-//    }
-//
-//    @GetMapping(path = "/all")
-//    public @ResponseBody
-//    List<Catalog> getAll() {
-//        return Catalog.getAll();
-//    }
-//
+
+     @GetMapping(path = "/{id}")
+     public @ResponseBody
+     String getById(@PathVariable Integer id) {
+        return "enter your username"+id;
+      }
+ 
+     @GetMapping(path = "/all")
+     public @ResponseBody
+     List<Catalog> getAll() {
+         return Catalog.getAll();
+     }
+  
     @PostMapping
     public CatalogDto create(@RequestBody CatalogDto catalogDto) {
         return catalogService.create(catalogDto);
     }
-//
-//    @PutMapping
-//    public Catalog update(@RequestBody Catalog dto) {
-//        return Catalog.update(dto);
-//    }
-//
-//    @DeleteMapping(path = "/{id}")
-//    public @ResponseBody
-//    String deleteById(@PathVariable Integer id) {
-//        Catalog.deleteById(id);
-//        return ControllerMessages.DELETED.name() + id;
-//    }
+ 
+     @PutMapping
+     public Catalog update(@RequestBody Catalog dto) {
+         return Catalog.update(dto);
+    }
+ 
+     @DeleteMapping(path = "/{id}")
+     public @ResponseBody
+     String deleteById(@PathVariable Integer id) {
+         Catalog.deleteById(id);
+         return ControllerMessages.DELETED.name() + id;
+     }
 
 }
